773fc6e90c28f2774587fd091ddbe693b786a109
chris <teihyung@gmail.com>
2025-01-26 16:03:19 -0800

    test push for CI/CD pipeline and updated environment setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config: any , context: any) => {
+        // Enable polling based on env variable being set
+        if(process.env.NEXT_WEBPACK_USEPOLLING) {
+            config.watchOptions = {
+                poll: 500,
+                aggregateTimeout: 300
+            }
+        }
+        return config
+    },
+}
 
-export default nextConfig;
+module.exports = nextConfig;
